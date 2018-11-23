4ee86dce817e8c3a25748cc07c7ec28c31d897b3
MohsAkhtar <mohs.akhtar@gmail.com>
2018-11-23 08:16:15 +0000

    Setting up settings page

--- src/App/AppProvider.js
+++ src/App/AppProvider.js
@@ -7,8 +7,21 @@ export class AppProvider extends React.Component {
     super(props);
     this.state = {
       page: 'dashboard',
+      ...this.savedSettings(),
       setPage: this.setPage
     };
+  }
+
+  // Check if user has used site before by looking at ls
+  savedSettings() {
+    let cryptoDashboardData = JSON.parse(
+      localStorage.getItem('cryptoDashboard')
+    );
+    // if we have not visited site before
+    if (!cryptoDashboardData) {
+      return { page: 'settings', firstVisit: true };
+    }
+    return {};
   }
 
   setPage = page => this.setState({ page });
--- src/App/index.js
+++ src/App/index.js
@@ -3,6 +3,7 @@ import './App.css';
 import AppLayout from './AppLayout';
 import AppBar from './AppBar';
 import { AppProvider } from './AppProvider';
+import Settings from '../Settings';
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
       <AppLayout>
         <AppProvider>
           <AppBar />
+          <Settings />
         </AppProvider>
       </AppLayout>
     );
