bf1fd397da4b41bb1050b6780147ded72c375130
MohsAkhtar <mohs.akhtar@gmail.com>
2018-11-23 13:06:21 +0000

    Filtering works from fuzzy search

--- src/Settings/Search.js
+++ src/Settings/Search.js
@@ -2,6 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 import { backgroundColor2, fontSize2 } from '../Shared/Styles';
 import { AppContext } from '../App/AppProvider';
+import _ from 'lodash';
+import fuzzy from 'fuzzy';
 
 const SearchGrid = styled.div`
   display: grid;
@@ -17,9 +19,33 @@ const SearchInput = styled.input`
   place-self: center left;
 `;
 
+// debounce is a lodash function that stops too many events firing off at once, telling it to wait 500ms before applying filter
+const handleFilter = _.debounce((inputValue, coinList, setFilteredCoins) => {
+  // Get all coin symbols
+  let coinSymbols = Object.keys(coinList);
+  // Get all the coin names, map symbol to name
+  let coinNames = coinSymbols.map(sym => coinList[sym].CoinName);
+  // Get all the coins and concat the name
+  let allStringToSearch = coinSymbols.concat(coinNames);
+  // fuzzy is used to do fuzzy search. It returns an object so needs to be converted to string
+  let fuzzyResults = fuzzy
+    .filter(inputValue, allStringToSearch, {})
+    .map(result => result.string);
+  // _.pickBy picks from the object a list of keys based on a callback function
+  let filteredCoins = _.pickBy(coinList, (result, symbolKey) => {
+    let coinName = result.CoinName;
+    return (
+      _.includes(fuzzyResults, symbolKey) || _.includes(fuzzyResults, coinName)
+    );
+  });
+  console.log(filteredCoins);
+  // set filtered coins to what we have filtered from above
+  setFilteredCoins(filteredCoins);
+}, 500);
+
 function filterCoins(e, setFilteredCoins, coinList) {
   let inputValue = e.target.value;
-  console.log(inputValue);
+  handleFilter(inputValue, coinList, setFilteredCoins);
 }
 
 export default function() {
