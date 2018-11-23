4b6948b02f62ca1eb95ec5e85aaab8c4825a31fc
MohsAkhtar <mohs.akhtar@gmail.com>
2018-11-23 09:31:29 +0000

    Setting up layout for favourite coins

--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -7,19 +7,20 @@ export const CoinGridStyled = styled.div`
   display: grid;
   grid-template-columns: repeat(5, 1fr);
   grid-gap: 15px;
+  margin-top: 40px;
 `;
 
-function getCoinsToDisplay(coinList) {
+function getCoinsToDisplay(coinList, topSection) {
   // don't want to display all 1000 images
-  return Object.keys(coinList).slice(0, 100);
+  return Object.keys(coinList).slice(0, topSection ? 10 : 100);
 }
 
-export default function() {
+export default function({ topSection }) {
   return (
     <AppContext.Consumer>
       {({ coinList }) => (
         <CoinGridStyled>
-          {getCoinsToDisplay(coinList).map(coinKey => (
+          {getCoinsToDisplay(coinList, topSection).map(coinKey => (
             <CoinTile coinKey={coinKey} />
           ))}
         </CoinGridStyled>
