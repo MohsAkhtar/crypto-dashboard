b0eba5eba7202b01291130e6213f773f7d77f805
MohsAkhtar <mohsin.akhtar0@gmail.com>
2018-11-26 15:34:55 +0000

    Updates

---
+++ precache-manifest.5f207e4f763d6bf0af3fd5ba85ff3497.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ca523a0e76b36b7ceb2",
+    "url": "/crypto-dashboard/static/js/runtime~main.8ca523a0.js"
+  },
+  {
+    "revision": "877fa6b34f8091b5c94a",
+    "url": "/crypto-dashboard/static/js/main.877fa6b3.chunk.js"
+  },
+  {
+    "revision": "99c2cde4a511c6e4b2d5",
+    "url": "/crypto-dashboard/static/js/1.99c2cde4.chunk.js"
+  },
+  {
+    "revision": "877fa6b34f8091b5c94a",
+    "url": "/crypto-dashboard/static/css/main.5546d54f.chunk.css"
+  },
+  {
+    "revision": "77cc1ce9f0667fe131da7256dfcb105b",
+    "url": "/crypto-dashboard/index.html"
+  }
+];
